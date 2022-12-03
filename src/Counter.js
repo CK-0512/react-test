086bd5078f75c4191e6c81475100c931d6a813e7
basdzxqwe <ock6166@naver.com>
2022-12-03 17:24:42 +0900

    리액트 useState, useEffect

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+const Counter = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    }
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    }
+
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    return () => {
+        console.log('cleanup');
+        console.log(name);
+    }
+    
+  }, []);
+    
+  return (
+    <>
+      <div>
+        <input value={name} onChange={onChangeName}></input>
+        <input value={nickname} onChange={onChangeNickname}></input>
+      </div>
+    </>
+  );
+}
+
+export default Counter;
