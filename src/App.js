31373f4003bfac4a27938e583bd2079dd1bd7294
basdzxqwe <ock6166@naver.com>
2022-12-07 20:56:05 +0900

    useMemo추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import StateEx from "./StateEx"
 import EffectEx from "./EffectEx";
 import ContextEx from "./ContextEx";
 import ReducerEx from "./ReducerEx";
+import MemoEx from "./MemoEx";
 
 const App = () => {
   return (
     <>
-      <StateEx />
+      {/* <StateEx />
       <EffectEx />
       <ContextEx />
-      <ReducerEx />
+      <ReducerEx /> */}
+      <MemoEx />
     </>
   );
 }
